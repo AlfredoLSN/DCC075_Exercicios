8d54c8452c139c5f0361e897422453a77f42caa6
Alfredo Lucas <alfredoliu14@live.com>
2025-05-22 14:31:11 -0300

    Add comment about IA

--- feistel/feistel.py
+++ feistel/feistel.py
@@ -3,6 +3,8 @@ from struct import pack
 from base64 import b64encode, b64decode
 
 
+# OBS: Foi utilizado IA para o entendimento do funcionamento de XOR e rotação de bits em python
+
 #Essa funcao recebe como parametro uma mensagem e o tamanho do bloco do algoritmo e rotarna uma lista com os blocos separados de acordo com o tamanho do bloco
 def divideEmBlocos(mensagem, tamanho_bloco):
         blocos = []
